f5b36125ecbefa60942a97215b606c28fb23f1b3
bolilloxd <34169937+bolilloxd@users.noreply.github.com>
2022-01-11 13:57:13 -0600

    header y footer php

--- header.php
+++ header.php
@@ -4,27 +4,27 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap - Prebuilt Layout</title>
+	<title>Danka | Inicio </title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
 
    </head>
   <body style="background-color:#000000	" > <!----Fondooooooooooooooo ---->
-    <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #000;"> <a class="navbar-brand" href="main.html">
+    <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #000;"> <a class="navbar-brand" href="main.php">
 		<img src="images/dankalogo_cuadrado.png" width="60" height="60" alt="">
 		</a>
 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 	    <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 	      <ul class="navbar-nav w-100 justify-content-around">
 	        <!--	<li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li> -->
-	        <li class="nav-item"> <a class="nav-link" href="servicios.html" style="color: red ;" ><b>Servicios</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="Influencers.html" style="color: cyan;" ><b>Influencers</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="imagenpp.html"  style="color: yellow;"><b>Imagen PP</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="seo_sem.html"  style="color: cyan; "><b>SEO/SEM</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="moda_arte.html" style="color: red;"><b>Moda y Arte</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="content_mk.html" style="color: cyan;"><b>Content MK</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="open_mind.html" style="color: yellow;"><b>Open Mind</b></a> </li>
+	        <li class="nav-item"> <a class="nav-link" href="servicios.php" style="color: red ;" ><b>Servicios</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="Influencers.php" style="color: cyan;" ><b>Influencers</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="imagenpp.php"  style="color: yellow;"><b>Imagen PP</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="seo_sem.php"  style="color: cyan; "><b>SEO/SEM</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="moda_arte.php" style="color: red;"><b>Moda y Arte</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="content_mk.php" style="color: cyan;"><b>Content MK</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="open_mind.php" style="color: yellow;"><b>Open Mind</b></a> </li>
 	        
           </ul>
       </div>
--- servicios.php
+++ servicios.php
@@ -27,25 +27,7 @@ z-index:100;
 }  
 </style>
   </head>
-  <body style="background-color:#000000	" > <!----Fondooooooooooooooo ---->
-    <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #000;"> <a class="navbar-brand" href="main.html">
-		<img src="images/dankalogo_cuadrado.png" width="60" height="60" alt="">
-		</a>
-	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-	    <div class="collapse navbar-collapse" id="navbarSupportedContent1">
-	      <ul class="navbar-nav w-100 justify-content-around">
-	        <!--	<li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li> -->
-	        <li class="nav-item"> <a class="nav-link" href="servicios.html"><b>Servicios</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="Influencers.html"><b>Influencers</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="imagenpp.html"><b>Imagen PP</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="seo_sem.html"><b>SEO/SEM</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="moda_arte.html"><b>Moda y Arte</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="content_mk.html"><b>Content MK</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="open_mind.html"><b>Open Mind</b></a> </li>
-	        
-          </ul>
-      </div>
-  </nav>
+  <?php include 'header.php'; ?>
 
 <section>
 	<div class="col-sm-12 col-md-12 col-lg-12 align-content-center" > 
