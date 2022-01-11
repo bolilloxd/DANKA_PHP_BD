d9d1665e735c5662b7d6b78eb2b8a8d79a3efa17
bolilloxd <34169937+bolilloxd@users.noreply.github.com>
2022-01-11 12:59:23 -0600

    Primeros cambios

--- header.php
+++ header.php
@@ -9,32 +9,25 @@
     <!-- Bootstrap -->
     <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
 
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #000;"> <a class="navbar-brand" href="#">
+   </head>
+  <body style="background-color:#000000	" > <!----Fondooooooooooooooo ---->
+    <nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #000;"> <a class="navbar-brand" href="main.html">
 		<img src="images/dankalogo_cuadrado.png" width="60" height="60" alt="">
 		</a>
 	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1" aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
 	    <div class="collapse navbar-collapse" id="navbarSupportedContent1">
 	      <ul class="navbar-nav w-100 justify-content-around">
 	        <!--	<li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li> -->
-	        <li class="nav-item"> <a class="nav-link" href="#"><b>Servicios</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>Influencers</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>Imagen PP</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>SEO/SEM</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>Moda y Arte</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>Content MK</b></a> </li>
-			<li class="nav-item"> <a class="nav-link" href="#"><b>Open Mind</b></a> </li>
+	        <li class="nav-item"> <a class="nav-link" href="servicios.html" style="color: red ;" ><b>Servicios</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="Influencers.html" style="color: cyan;" ><b>Influencers</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="imagenpp.html"  style="color: yellow;"><b>Imagen PP</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="seo_sem.html"  style="color: cyan; "><b>SEO/SEM</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="moda_arte.html" style="color: red;"><b>Moda y Arte</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="content_mk.html" style="color: cyan;"><b>Content MK</b></a> </li>
+			<li class="nav-item"> <a class="nav-link" href="open_mind.html" style="color: yellow;"><b>Open Mind</b></a> </li>
 	        
           </ul>
       </div>
-  </nav>
+	</nav>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) --> 
-    <script src="js/jquery-3.4.1.min.js"></script>
 
-    <!-- Include all compiled plugins (below), or include individual files as needed --> 
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap-4.4.1.js"></script>
-  </body>
-</html>
